5510567ff32fc9de5d208dac6f17b9d668679e53
hugosenari <hugosenari@gmail.com>
2022-06-26 19:52:00 -0300

    chore: re index omnicollider

--- 0_4_1/flake.nix
+++ 0_4_1/flake.nix
@@ -7,11 +7,11 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
-  inputs.src-omnicollider-0_2_0.flake = false;
-  inputs.src-omnicollider-0_2_0.ref   = "refs/tags/0.2.0";
-  inputs.src-omnicollider-0_2_0.owner = "vitreo12";
-  inputs.src-omnicollider-0_2_0.repo  = "omnicollider";
-  inputs.src-omnicollider-0_2_0.type  = "github";
+  inputs.src-omnicollider-0_4_1.flake = false;
+  inputs.src-omnicollider-0_4_1.ref   = "refs/tags/0.4.1";
+  inputs.src-omnicollider-0_4_1.owner = "vitreo12";
+  inputs.src-omnicollider-0_4_1.repo  = "omnicollider";
+  inputs.src-omnicollider-0_4_1.type  = "github";
   
   inputs."cligen".owner = "nim-nix-pkgs";
   inputs."cligen".ref   = "master";
@@ -32,13 +32,13 @@
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
   let 
     lib  = flakeNimbleLib.lib;
-    args = ["self" "nixpkgs" "flakeNimbleLib" "src-omnicollider-0_2_0"];
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-omnicollider-0_4_1"];
     over = if builtins.pathExists ./override.nix 
            then { override = import ./override.nix; }
            else { };
   in lib.mkRefOutput (over // {
     inherit self nixpkgs ;
-    src  = deps."src-omnicollider-0_2_0";
+    src  = deps."src-omnicollider-0_4_1";
     deps = builtins.removeAttrs deps args;
     meta = builtins.fromJSON (builtins.readFile ./meta.json);
   } );
